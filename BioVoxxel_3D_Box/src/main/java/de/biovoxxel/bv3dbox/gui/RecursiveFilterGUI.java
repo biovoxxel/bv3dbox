ca5a63fa8feef6488621519b01a2fed5293c1c69
Jan Brocher <jan.brocher@biovoxxel.de>
2022-01-29 20:11:42 +0100

    add labels to recursive filter GUI

--- BioVoxxel_3D_Box/src/main/java/de/biovoxxel/bv3dbox/gui/RecursiveFilterGUI.java
+++ BioVoxxel_3D_Box/src/main/java/de/biovoxxel/bv3dbox/gui/RecursiveFilterGUI.java
@@ -11,22 +11,22 @@ import ij.ImagePlus;
 @Plugin(type = Command.class, menuPath = "Plugins>BioVoxxel 3D Box>Recursive Filter (2D/3D)")
 public class RecursiveFilterGUI extends DynamicCommand {
 
-	@Parameter(required = true, label = "", description = "")
+	@Parameter(required = true, label = "Image", description = "")
 	ImagePlus current_image_plus;
 	
-	@Parameter(required = true, label = "", description = "", choices = {"Median", "Gaussian"})
+	@Parameter(required = true, label = "Filter", description = "", choices = {"Median", "Gaussian"})
 	String filter_method = "Gaussian";
 	
-	@Parameter(required = true, label = "", description = "", min = "0.5", max = "20.0", stepSize = "0.5")
+	@Parameter(required = true, label = "Radius", description = "", min = "0.5", max = "20.0", stepSize = "0.5")
 	Double recursiveRadius = 1.0;
 	
-	@Parameter(required = true, label = "", description = "", stepSize = "10", max = "200")
+	@Parameter(required = true, label = "Iteration", description = "", stepSize = "10", max = "200")
 	Integer iterations = 10;
 	
 
 	
 
-	@Override
+
 	public void run() {
 		
 		BVRecursiveFilter bvrf = new BVRecursiveFilter(current_image_plus);

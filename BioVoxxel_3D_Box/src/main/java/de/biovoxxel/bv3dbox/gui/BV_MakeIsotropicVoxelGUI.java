4b2ba13ec826ed3026f3d2fba6a6604b56169a95
Jan Brocher <biovoxxel@users.noreply.github.com>
2023-06-12 20:05:43 +0200

    add make voxel isotropic

--- BioVoxxel_3D_Box/src/main/java/de/biovoxxel/bv3dbox/gui/BV_MakeIsotropicVoxelGUI.java
+++ BioVoxxel_3D_Box/src/main/java/de/biovoxxel/bv3dbox/gui/BV_MakeIsotropicVoxelGUI.java
@@ -6,7 +6,7 @@ import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
-import de.biovoxxel.bv3dbox.plugins.BV_MakeIsotropicImage;
+import de.biovoxxel.bv3dbox.plugins.BV_MakeIsotropicVoxel;
 import de.biovoxxel.bv3dbox.utilities.BV3DBoxUtilities;
 import de.biovoxxel.bv3dbox.utilities.BV3DBoxUtilities.LutNames;
 
@@ -16,12 +16,16 @@ import ij.measure.Calibration;
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij2.CLIJ2;
 
-@Plugin(type = Command.class, menuPath = "Plugins>BioVoxxel 3D Box>Segmentation>Make 3D Image Isotropic")
-public class BV_MakeIsotropicImageGUI implements Command {
+@Plugin(type = Command.class, menuPath = "Plugins>BioVoxxel 3D Box>Segmentation>Make Voxel Isotropic")
+public class BV_MakeIsotropicVoxelGUI implements Command {
 
 	
 	@Parameter(label = "Image", required = true)
 	ImagePlus inputImagePlus;
+	
+	@Parameter(label = "Voxel size [units]", required = true)
+	Float finalVoxelSize = 1f;
+	
 	
 	@Override
 	public void run() {
@@ -30,10 +34,10 @@ public class BV_MakeIsotropicImageGUI implements Command {
 			
 			CLIJ2 clij2 = CLIJ2.getInstance();
 			clij2.clear();
-			BV_MakeIsotropicImage bvmii = new BV_MakeIsotropicImage(clij2, inputImagePlus);
-			ClearCLBuffer isotropic_image = bvmii.makeIsotropic(clij2, inputImagePlus);
+			BV_MakeIsotropicVoxel bvmii = new BV_MakeIsotropicVoxel(clij2, inputImagePlus);
+			ClearCLBuffer isotropic_image = bvmii.makeIsotropic(clij2, inputImagePlus, finalVoxelSize);
 			
-			double pixelSize = inputImagePlus.getCalibration().pixelWidth;
+//			double pixelSize = inputImagePlus.getCalibration().pixelWidth;
 			
 			ImagePlus isotropicImagePlus = BV3DBoxUtilities.pullImageFromGPU(clij2, isotropic_image, true, LutNames.GRAY);
 			isotropic_image.close();
@@ -41,7 +45,7 @@ public class BV_MakeIsotropicImageGUI implements Command {
 			isotropicImagePlus.setTitle("iso_" + WindowManager.getUniqueName(inputImagePlus.getTitle()));
 				
 			Calibration cal = new Calibration();
-			cal.pixelWidth = cal.pixelHeight = cal.pixelDepth = pixelSize;
+			cal.pixelWidth = cal.pixelHeight = cal.pixelDepth = finalVoxelSize;
 			cal.setUnit(inputImagePlus.getCalibration().getUnit());	
 			
 			isotropicImagePlus.setCalibration(cal);
--- BioVoxxel_3D_Box/src/main/java/de/biovoxxel/bv3dbox/plugins/BV_MakeIsotropicVoxel.java
+++ BioVoxxel_3D_Box/src/main/java/de/biovoxxel/bv3dbox/plugins/BV_MakeIsotropicVoxel.java
@@ -5,14 +5,14 @@ import ij.measure.Calibration;
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij2.CLIJ2;
 
-public class BV_MakeIsotropicImage {
+public class BV_MakeIsotropicVoxel {
 	
 	ImagePlus inputImagePlus;
 	ClearCLBuffer input_image;
 	float[] calibration = new float[3];
 	CLIJ2 clij2;
 	
-	public BV_MakeIsotropicImage(CLIJ2 clij2, ImagePlus inputImagePlus) {
+	public BV_MakeIsotropicVoxel(CLIJ2 clij2, ImagePlus inputImagePlus) {
 		
 		if (clij2 == null) {
 			clij2 = CLIJ2.getInstance();
@@ -26,7 +26,7 @@ public class BV_MakeIsotropicImage {
 	}
 	
 	
-	public ClearCLBuffer makeIsotropic(CLIJ2 clij2, ImagePlus image) {
+	public ClearCLBuffer makeIsotropic(CLIJ2 clij2, ImagePlus image, float finalVoxelSize) {
 				
 		Calibration cal = image.getCalibration();
 		
@@ -35,15 +35,14 @@ public class BV_MakeIsotropicImage {
 		calibration[2] = (float) cal.pixelDepth;
 		
 		
-		return makeIsotropic(clij2, input_image, calibration);
+		return makeIsotropic(clij2, input_image, calibration, finalVoxelSize);
 		
 	}
 	
-	public ClearCLBuffer makeIsotropic(CLIJ2 clij2, ClearCLBuffer input_image, float[] calibration) {
+	public ClearCLBuffer makeIsotropic(CLIJ2 clij2, ClearCLBuffer input_image, float[] calibration, float finalVoxelSize) {
 		
-		ClearCLBuffer output_image = createOutputBufferFromSource(input_image);
-		
-		clij2.makeIsotropic(input_image, output_image, calibration[0], calibration[1], calibration[2], calibration[0]);
+		ClearCLBuffer output_image = createOutputBufferFromSource(input_image, finalVoxelSize);
+		clij2.makeIsotropic(input_image, output_image, calibration[0], calibration[1], calibration[2], finalVoxelSize);
 		
 		input_image.close();
 		
@@ -52,11 +51,11 @@ public class BV_MakeIsotropicImage {
 	
 	
 	
-	public ClearCLBuffer createOutputBufferFromSource(ClearCLBuffer input) {
+	public ClearCLBuffer createOutputBufferFromSource(ClearCLBuffer input, float finalVoxelSize) {
 
-        float scale1X = (float) (calibration[0] / calibration[0]);
-        float scale1Y = (float) (calibration[1] / calibration[0]);
-        float scale1Z = (float) (calibration[2] / calibration[0]);
+        float scale1X = (float) (calibration[0] / finalVoxelSize);
+        float scale1Y = (float) (calibration[1] / finalVoxelSize);
+        float scale1Z = (float) (calibration[2] / finalVoxelSize);
 
         return clij2.create(
                 (long) (input.getWidth() * scale1X),
